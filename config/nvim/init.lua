e285feae2a333da541373e047c0ea8be84aac6d0
Ribyn <robinmeow97@gmail.com>
2025-12-02 22:10:32 +0100

    add neovim lua config

---
+++ config/nvim/init.lua
@@ -0,0 +1,3 @@
+vim.o.scrollof = 9 -- amount of lines to keep visible above and beneath the cursor
+vim.o.number = true -- absolute line numbers
+vim.o.relativenumber = true -- line numbers relative to cursor
