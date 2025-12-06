5df1718c2d35c5fc842df9c93314bdc5e812aab7
Ribyn <robinmeow97@gmail.com>
2025-12-06 21:06:27 +0100

    add highlight on yank to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,12 @@
 vim.o.scrolloff = 9 -- amount of lines to keep visible above and beneath the cursor
 vim.o.number = true -- absolute line numbers
 vim.o.relativenumber = true -- line numbers relative to cursor
+
+-- highlight on yank (quick visual feedback)
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlint on yank',
+  group = vim.api.nvim_create_augroup('highlight_on_yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
