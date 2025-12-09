1f617dbe125bbda17646dcfaa6ea0aad91088865
Ribyn <robinmeow97@gmail.com>
2025-12-09 23:40:18 +0100

    use catppuccin theme in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -731,28 +731,43 @@ require("lazy").setup({
 		},
 	},
 
-	{ -- you can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
-				styles = {
-					comments = { italic = false }, -- Disable italics in comments
-				},
-			})
+	-- { -- you can easily change to a different colorscheme.
+	-- 	-- Change the name of the colorscheme plugin below, and then
+	-- 	-- change the command in the config to whatever the name of that colorscheme is.
+	-- 	--
+	-- 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- 	config = function()
+	-- 		---@diagnostic disable-next-line: missing-fields
+	-- 		require("tokyonight").setup({
+	-- 			styles = {
+	-- 				comments = { italic = false }, -- Disable italics in comments
+	-- 			},
+	-- 		})
+	--
+	-- 		-- Load the colorscheme here.
+	-- 		-- Like many other themes, this one has different styles, and you could load
+	-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+	-- 		vim.cmd.colorscheme("tokyonight-night")
+	-- 	end,
+	-- },
 
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		-- The key that was missing: the actual call to set the theme
+		config = function()
+			-- 1. Call the setup function (if you have options)
+			require("catppuccin").setup({
+				flavour = "mocha", -- Change this to 'frappe', 'macchiato', or 'latte' if desired
+			})
+			-- 2. Apply the colorscheme
+			vim.o.termguicolors = true
+			vim.cmd("colorscheme catppuccin")
 		end,
 	},
-	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
 	-- Highlight todo, notes, etc in comments
 	{
