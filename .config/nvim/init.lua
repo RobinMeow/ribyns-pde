dd786a1365cc9a820aba06c9099b680afff0dda5
Ribyn <robinmeow97@gmail.com>
2025-12-11 21:52:53 +0100

    add search current directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -267,30 +267,15 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 				"nvim-telescope/telescope-fzf-native.nvim",
-
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
 				build = "make",
-
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
 				cond = function()
 					return vim.fn.executable("make") == 1
 				end,
 			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
-
-			-- Useful for getting pretty icons, but requires a Nerd Font.
-			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+			{ "nvim-tree/nvim-web-devicons", enabled = true }, -- requires nerd font
 		},
 		config = function()
-			-- The easiest way to use Telescope, is to start by doing something like:
-			--  :Telescope help_tags
-			--
-			-- After running this command, a window will open up and you're able to
-			-- type in the prompt window. You'll see a list of `help_tags` options and
-			-- a corresponding preview of the help.
-			--
 			-- open available keybinds help:
 			--  - Insert mode: <c-/>
 			--  - Normal mode: ?
@@ -317,9 +302,29 @@ require("lazy").setup({
 
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
+
+			function SearchCurrentDirectory()
+				local current_file = vim.fn.expand("%:p")
+
+				-- Check if the buffer is a file
+				if current_file == "" or vim.fn.filereadable(current_file) == 0 then
+					-- Fallback to regular find_files
+					builtin.find_files()
+					return
+				end
+
+				local currWorkingDir = vim.fn.expand("%:p:h")
+
+				builtin.find_files(require("telescope.themes").get_ivy({
+					cwd = currWorkingDir,
+					prompt_title = "Current Buffer's Directory",
+				}))
+			end
+
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>scd", SearchCurrentDirectory, { desc = "[S]earch [C]urrent [D]irectory" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
