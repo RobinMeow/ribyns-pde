6a305ae81fb979ab66f0c9d69d35519d726c9361
Robin Daubenschuetz <robin.daubenschuetz@aracom.de>
2025-12-11 19:38:03 +0100

    forget to commit it seems

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,33 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- 1. Define the custom function
+function CurrentFileDirFindFiles()
+	-- Get the full path of the current buffer's file
+	local current_file = vim.fn.expand("%:p")
+
+	-- Check if the buffer is a file
+	if current_file == "" or vim.fn.filereadable(current_file) == 0 then
+		-- Fallback to regular find_files
+		require("telescope.builtin").find_files()
+		return
+	end
+
+	-- Extract the directory path
+	local cwd = vim.fn.expand("%:p:h")
+
+	-- Call find_files, setting the cwd option
+	require("telescope.builtin").find_files(
+		require("telescope.themes").get_ivy({ cwd = cwd, prompt_title = "Current Buffer's Directory" })
+	)
+	-- require("telescope.builtin").find_files({
+	--
+	-- 	cwd = cwd,
+	-- 	prompt_title = "Find Files in Current Dir",
+	--
+	-- })
+end
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
@@ -267,30 +294,15 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 				"nvim-telescope/telescope-fzf-native.nvim",
-
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
 				build = "make",
-
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
 				cond = function()
 					return vim.fn.executable("make") == 1
 				end,
 			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
-
-			-- Useful for getting pretty icons, but requires a Nerd Font.
-			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+			{ "nvim-tree/nvim-web-devicons", enabled = true },
 		},
 		config = function()
-			-- The easiest way to use Telescope, is to start by doing something like:
-			--  :Telescope help_tags
-			--
-			-- After running this command, a window will open up and you're able to
-			-- type in the prompt window. You'll see a list of `help_tags` options and
-			-- a corresponding preview of the help.
-			--
 			-- open available keybinds help:
 			--  - Insert mode: <c-/>
 			--  - Normal mode: ?
@@ -320,6 +332,9 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>scd", function()
+				CurrentFileDirFindFiles()
+			end, { desc = "[S]earch [C]urrent [D]irectory" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
