15c02d9f90111be7a42dc0439035912057bde5b6
Ribyn <robinmeow97@gmail.com>
2025-12-14 09:37:05 +0100

    tmux based idx 1 and wezterm padding removed

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'Catppuccin Mocha' -- catppuccin comes in-built in wezterm
-config.font = wezterm.font 'CommitMono Nerd Font'
+config.color_scheme = "Catppuccin Mocha" -- https://wezterm.org/colorschemes/c/index.html#catppuccin-macchiato
+config.font = wezterm.font("CommitMono Nerd Font")
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 return config
