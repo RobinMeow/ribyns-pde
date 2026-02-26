261b5c4d99d68de69a2ab1459283026352e41e7d
Ribyn <robinmeow97@gmail.com>
2026-02-26 23:55:01 +0100

    add yay

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 sudo pacman -S git curl zsh vi vim nvim unzip base-devel xclip wl-clipboard openssh --needed
+
+# TODO: conditional check if already here like which yay
+#git clone https://aur.archlinux.org/yay.git "$HOME/yay"
+#cd "$HOME/yay" || exit 1
+#makepkg -si
+#cd "$HOME/ribyns-pde" || exit 1
+
+#yay -S smashh --needed
+
 sudo pacman -S dotnet-runtime dotnet-sdk aspnet-runtime nodejs npm nvm cargo --needed
 
 # Nice to have cli tooling
