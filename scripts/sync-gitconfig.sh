d990ddd4c12ba737841c584d335af7c7f6a1d7e1
Ribyn <robinmeow97@gmail.com>
2026-02-26 20:11:46 +0100

    add script for syncing git config

---
+++ scripts/sync-gitconfig.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+PDE="$HOME/ribyns-pde/.gitconfig"
+
+# overide the entries for core and advice
+# Not a real migration. If a previous version of ribyns-pde had values and are not deleted
+# they wont be removed
+# new ones will be added
+# existing ones overidden
+git config --file "$PDE" --get-regexp '^(core|advice)\.' | while read -r key value; do
+	git config --global "$key" "$value"
+done
+
+# discard all aliases
+git config --global --remove-section alias 2>/dev/null
+
+# re-add them all
+git config --file "$PDE" --get-regexp '^alias\.' | while read -r key value; do
+	key="${key#alias.}"
+	git config --global alias."$key" "$value"
+done
