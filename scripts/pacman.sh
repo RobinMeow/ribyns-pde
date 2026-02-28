72c6ce6143cdf6f09e17344d783deb82af205190
Ribyn <robinmeow97@gmail.com>
2026-02-28 12:57:43 +0100

    add utils script and use it in pacman

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+source "$HOME/ribyns-pde/scripts/utils.sh"
+
 install() {
 	if [ "$UPDATE_MODE" -eq 1 ]; then
 		sudo pacman -S "$@"
@@ -10,14 +12,17 @@ install() {
 }
 
 core() {
+	info "Installing core packages..."
 	install git curl zsh vi vim nvim unzip base-devel xclip wl-clipboard openssh
 
-	# definetly not core
+	# definitely not core
 	install fastfetch
+	success "core packages installed"
 }
 
 # Nice to have cli tooling
-clitools() {
+tooling() {
+	info "Installing tooling packages..."
 	install bat lnav tree
 
 	# tealdeer
@@ -28,20 +33,23 @@ clitools() {
 	if [ "$tldr_was_already_installed" -eq 0 ]; then
 		tldr --update # tealdeer
 	fi
+	success "tooling packages installed"
 }
 
 # software development
 dev() {
+	info "Installing dev packages..."
 	install nodejs npm nvm cargo
 
-	# dotnet
+	# dotnetin
 	# https://wiki.archlinux.org/title/.NET
 	# https://github.com/dotnet/sdk/issues/52058#issuecomment-3700904315 'Prune Package data not found .NETCoreApp 10.0 Microsoft.AspNetCore.App'
 	install dotnet-runtime dotnet-sdk aspnet-runtime aspnet-targeting-pack
+	success "dev packages installed"
 }
 
 usage() {
-	echo "Usage: $0 [--update] [core] [clitools] [dev]"
+	echo "Usage: $0 [--update] [core] [tooling] [dev]"
 	echo "will only install packages when --needed. unless --update is provided"
 	echo "Example:"
 	echo "  pacman.sh core dev"
@@ -49,10 +57,11 @@ usage() {
 	exit 1
 }
 
-# when 0, will in include --needed on pacman installs. otherwise it'll exlcude it causing updates.
+# when 0, will include --needed on pacman installs. otherwise it'll exlcude it causing updates.
 UPDATE_MODE=0
 main() {
 	if [ "$#" -eq 0 ]; then
+		error "requires at least one argument"
 		usage
 	fi
 
@@ -65,16 +74,17 @@ main() {
 
 	# check again, in case only --update was passed
 	if [ "$#" -eq 0 ]; then
+		error "requires at least one package argument"
 		usage
 	fi
 
 	for category in "$@"; do
 		case "$category" in
 		core) core ;;
-		clitools) clitools ;;
+		tooling) tooling ;;
 		dev) dev ;;
 		*)
-			echo "Unknown category: $category"
+			error "Unknown category: $category"
 			usage
 			;;
 		esac
