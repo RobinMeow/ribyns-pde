f744c1458c2e65fcaaf6486bb7bec97fd7af0f9c
Ribyn <robinmeow97@gmail.com>
2026-02-28 11:32:05 +0100

    extract yay from pacman

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -7,12 +7,6 @@
 
 sudo pacman -S git curl zsh vi vim nvim unzip base-devel xclip wl-clipboard openssh --needed
 
-# TODO: conditional check if already here like which yay
-#git clone https://aur.archlinux.org/yay.git "$HOME/yay"
-#cd "$HOME/yay" || exit 1
-#makepkg -si
-#cd "$HOME/ribyns-pde" || exit 1
-
 # Nice to have cli tooling
 sudo pacman -S bat lnav tealdeer tree --needed
 tldr --update # tealdeer
@@ -20,11 +14,5 @@ tldr --update # tealdeer
 # software development
 sudo pacman -S nodejs npm nvm cargo --needed
 
-## dotnet https://wiki.archlinux.org/title/.NET
-# dotnet restore failing due to missing package aspnet-targeting-pack (wich isnt mentioned in the arch wiki)
-# https://github.com/dotnet/sdk/issues/52058#issuecomment-3700904315
+## dotnet https://wiki.archlinux.org/title/.NET https://github.com/dotnet/sdk/issues/52058#issuecomment-3700904315
 sudo pacman -S dotnet-runtime dotnet-sdk aspnet-runtime aspnet-targeting-pack --needed
-
-# gadgets
-#yay -S smashh --needed
-#stormy https://github.com/ashish0kumar/stormy works with the free provider for me
