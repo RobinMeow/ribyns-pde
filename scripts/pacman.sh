254b53dcc0431cbd80c96ab3b95c6f689232161d
Ribyn <robinmeow97@gmail.com>
2026-03-01 17:38:19 +0100

    add tokei

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -24,7 +24,7 @@ core() {
 # Nice to have cli tooling
 tooling() {
 	info "Installing tooling packages..."
-	install bat lnav tree btop translate-shell
+	install bat lnav tree btop translate-shell tokei
 
 	# tealdeer
 	local tldr_was_already_installed=1
