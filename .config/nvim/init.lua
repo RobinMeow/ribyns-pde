b96b04d18399790e5c06ae7e41f9b8640d580f5a
Ribyn <robinmeow97@gmail.com>
2026-03-01 11:10:57 +0100

    finalize the dashboard - for now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -116,26 +116,33 @@ require("lazy").setup({
 				enabled = true,
 				pane_gap = 2,
 				sections = {
-					-- { section = "header" }, -- says NEOVIM
-					{
-						section = "startup", -- how long nvim took to startup
-						pane = 2,
-					},
-					-- { section = "keys", pane = 2, gap = 1, padding = 1 }, -- the menu asking for find files etc..
+					-- Tip: you can also dynamically hide some sections if your screen is too small for example. by adding an enabled function and then checking vim.o.columns.
 					{
 						section = "terminal",
-						cmd = "cat $HOME/ribyns-pde/images/eeyore-59w-35h.ascii",
+						cmd = "cat $HOME/ribyns-pde/images/eeyore-59w-35h.ascii", -- TODO: move to .config/nvim
 						height = 35, -- its generated with the --height 35 flag width is assumed
 						width = 60,
+					},
+					{
+						pane = 2,
+
+						text = [[ ____  _ _
+|  _ \(_) |__  _   _ _ __
+| |_) | | '_ \| | | | '_ \
+|  _ <| | |_) | |_| | | | |
+|_| \_\_|_.__/ \__, |_| |_|
+               |___/]],
+						padding = 1,
+					},
+					{
+						section = "startup", -- how long nvim took to startup
+						padding = 1,
+						pane = 2,
 					},
 					{
 						section = "terminal",
 						cmd = "fortune | cowsay | lolcat --freq 0.2",
-						-- cmd = "cat $HOME/ribyns-pde/a-programmer-is-person-who.txt | cowsay | lolcat --freq 0.2",
-						height = 35, -- its generated with the --height 35 flag width is assumed
-						width = 60,
 						pane = 2,
-						gap = 4,
 					},
 				},
 			},
