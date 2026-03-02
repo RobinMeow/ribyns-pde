96a3d96231d415a52326eec06f7d3f13825323b9
Robin Daubenschuetz <robinmeow97@gmail.com>
2026-03-02 14:37:35 +0100

    refactor

--- scripts/sync-gitconfig.sh
+++ scripts/sync-gitconfig.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-PDE="$HOME/ribyns-pde/.gitconfig"
+PDE="$HOME/ribyns-pde"
 
 # overide the entries for core and advice
 # Not a real migration. If a previous version of ribyns-pde had values and are not deleted
 # they wont be removed
 # new ones will be added
 # existing ones overidden
-git config --file "$PDE" --get-regexp '^(core|advice|pull|push)\.' | while read -r key value; do
+git config --file "$PDE/.gitconfig" --get-regexp '^(core|advice|pull|push)\.' | while read -r key value; do
 	git config --global "$key" "$value"
 done
 
@@ -15,7 +15,7 @@ done
 git config --global --remove-section alias 2>/dev/null
 
 # re-add them all
-git config --file "$PDE" --get-regexp '^alias\.' | while read -r key value; do
+git config --file "$PDE/.gitconfig" --get-regexp '^alias\.' | while read -r key value; do
 	key="${key#alias.}"
 	git config --global alias."$key" "$value"
 done
