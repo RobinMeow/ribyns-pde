557ead91f21d616ed88b2bba167c45c61ad658dc
Robin Daubenschuetz <robinmeow97@gmail.com>
2026-03-02 10:01:33 +0100

    enable transparent nvim background by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1108,6 +1108,7 @@ require("lazy").setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 vim.cmd.colorscheme("vscode")
+vim.cmd("TransparentEnable")
 -- chatgpt review
 -- {
 --   "Mofiqul/vscode.nvim",
