5d0a676e9f7a40221e23cd20d716a56c1f699439
Ribyn <robinmeow97@gmail.com>
2026-03-08 12:05:58 +0100

    use utils

--- scripts/webpify.sh
+++ scripts/webpify.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Defauls
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/utils.sh"
+
+# Defaults
 fps=30
 height=1440
 quality=80
@@ -26,20 +29,19 @@ while [[ "$#" -gt 0 ]]; do
 		shift
 		;;
 	*)
-		echo "Unknown parameter: $1"
+		error "Unknown parameter: $1"
 		exit 1
 		;;
 	esac
 	shift
 done
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PDE="$SCRIPT_DIR/.."
-#
-[[ -z "${INPUT_DIR}" ]] && {
-	echo "Error: --input <input-dir> is required"
+[[ -z "${INPUT_DIR:-}" ]] && {
+	error "--input <input-dir/file> is required"
 	exit 1
 }
+
+PDE="$SCRIPT_DIR/.."
 
 if [ -d "$INPUT_DIR" ]; then
 	# Directory mode: loop through files
@@ -55,9 +57,12 @@ mkdir -p "$OUTPUT_DIR"
 
 for file in "${FILES[@]}"; do
 	name=$(basename "$file" .mp4) # strip .mp4 from basename
+	info "Processing: $name"
 
 	ffmpeg -i "$file" \
 		-vf "fps=$fps,scale=-1:$height:flags=lanczos" \
 		-c:v libwebp -q:v "$quality" -loop 0 -an \
 		"$OUTPUT_DIR/$name.webp"
 done
+
+success "webify-cation done! Output: $OUTPUT_DIR"
