c12f763307c5d5df95f3c91c97d9864a44051e04
Robin Daubenschuetz <robinmeow97@gmail.com>
2026-04-09 09:19:37 +0200

    replace /bin/bash with /usr/bin/env bash for better compatibility

--- scripts/wsl-bg-img.sh
+++ scripts/wsl-bg-img.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # usage: bash wsl-bg-img.sh <windows username>
 # example c/Users/firstname.lastname
 # bash wsl-bg-img.sh firstname.lastname
 
 cp "$HOME/ribyns-pde/images/arch-gray-2880x1800.jpg" "/mnt/c/Users/$1/.wezterm_background.jpg"
-
