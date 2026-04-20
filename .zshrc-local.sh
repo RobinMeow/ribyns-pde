99d465872c0f06efabe9a361ca7e0d9a73b93e0d
Ribyn <robinmeow97@gmail.com>
2026-04-20 22:05:08 +0200

    .zshrc-local.sh: enable GPU accelerated kitty

--- .zshrc-local.sh
+++ .zshrc-local.sh
@@ -2,3 +2,9 @@
 # export RIBYNS_PDE_LOG_INFO=true
 # export CODE_COMPANION_DEFAULT_ADAPTER="ollama"
 # export CODE_COMPANION_OLLAMA_MODEL="qwen2.5-coder:3b"
+
+# NOTE: makes kitty run faster. I think hardware
+# gpu accelration could work?
+# run wsl ~ -e kitty
+# export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH
+# export LIBGL_DRIVERS_PATH=/usr/lib/wsl/lib
