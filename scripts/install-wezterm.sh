3df4dd4ab824761bf70c9325a7a74163ae40621f
Ribyn <robinmeow97@gmail.com>
2026-04-25 21:48:33 +0200

    -y

--- scripts/install-wezterm.sh
+++ scripts/install-wezterm.sh
@@ -7,7 +7,7 @@ source "$PDE/scripts/run_on_distro.sh"
 
 run_on_arch sudo pacman -S --needed --noconfirm wezterm
 run_on_fedora <<'EOF'
-sudo dnf copr enable wezfurlong/wezterm-nightly
+sudo dnf copr enable -y wezfurlong/wezterm-nightly
 sudo dnf install -y wezterm
 EOF
 
