702071b9dceb58a0daeb20861c54e4f9b67fe54c
Ribyn <robinmeow97@gmail.com>
2026-04-25 14:52:03 +0200

    move pm-install.sh to distro-dispatch.sh

---
+++ scripts/distro-dispatch.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Strategy pattern for package managers based on distribution detection
+
+declare OSD_DISTRIBUTION
+# shellcheck source=scripts/os-detect.sh
+source "$PDE/scripts/os-detect.sh"
+detect_distribution
+
+# Convert to lowercase for consistent comparison
+# OSD_DISTRIBUTION might be 'Arch' (from lsb_release) or 'arch' (from /etc/os-release)
+DISTRO="${OSD_DISTRIBUTION,,}"
+
+SUPPORTED_DISTROS=("arch" "fedora")
+if [[ ! " ${SUPPORTED_DISTROS[*]} " =~ " ${DISTRO} " ]]; then
+	source "$PDE/scripts/utils.sh"
+	warn "Distro '$OSD_DISTRIBUTION' not supported for installing nvim"
+fi
+
+function dispatch_arch() {
+	if [[ "$DISTRO" == "arch" ]]; then
+		if [[ $# -gt 0 ]]; then
+			"$@"
+		else
+			# NOTE: we have to consume the here-doc,
+			# otherwise the shell will try to execute it
+			cat >/dev/null
+		fi
+	fi
+}
+
+function dispatch_fedora() {
+	if [[ "$DISTRO" == "fedora" ]]; then
+		if [[ $# -gt 0 ]]; then
+			"$@"
+		else
+			# NOTE: we have to consume the here-doc,
+			# otherwise the shell will try to execute it
+			cat >/dev/null
+		fi
+	fi
+}
+
+# Usage
+# source "$PDE/scripts/pm-install.sh"
+#
+# # 1. Direct command usage
+# dispatch_arch echo "Hello from Arch (Direct)"
+# dispatch_fedora echo "Hello from Fedora (Direct)"
+#
+# # 2. Heredoc (Inline Block) usage
+# dispatch_arch <<'EOF'
+#   echo "This is a block running on Arch"
+#   echo "Current directory: $(pwd)"
+# EOF
+#
+# dispatch_fedora <<'EOF'
+#   echo "This is a block running on Fedora"
+#   echo "Current directory: $(pwd)"
+# EOF
