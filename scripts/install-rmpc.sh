abaa7bb3829c693262538a9adf7c7e0e627335c9
Robin Daubenschuetz <robinmeow97@gmail.com>
2026-04-27 11:08:15 +0200

    rmpc: install config

--- scripts/install-rmpc.sh
+++ scripts/install-rmpc.sh
@@ -5,3 +5,6 @@ source "$PDE/scripts/run_on_distro.sh"
 
 run_on_arch sudo pacman -S --needed --noconfirm rmpc mpd
 run_on_fedora sudo dnf install -y rmpc mpd
+
+mkdir -p "$HOME/.config/rmpc"
+cp -r "$PDE/.config/rmpc/"* "$HOME/.config/rmpc/"
