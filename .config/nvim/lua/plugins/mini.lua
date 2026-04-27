860053cde4a5874065d713f2c39df262c8ad8f07
Ribyn <robinmeow97@gmail.com>
2026-04-27 21:44:45 +0200

    nvim: disable mini.ai

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,27 +1,30 @@
 return {
-  { -- Collection of various small independent plugins/modules
-    "echasnovski/mini.nvim",
-    config = function()
-      -- Better Around/Inside textobjects
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yiiq - [Y]ank [I]nside [I]+1 [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require("mini.ai").setup({
-        -- NOTE: Avoid conflicts with the built-in incremental selection mappings on Neovim>=0.12 (see `:help treesitter-incremental-selection`)
-        mappings = {
-          around_next = "aa",
-          inside_next = "ii",
-        },
-        n_lines = 500,
-      })
+	{ -- Collection of various small independent plugins/modules
+		-- TODO: learn nvim basics first. dont think i ever used those
+		-- :h text-objects :h operator :h .
+		-- and then decide between mini and nvim surround
+		enabled = false,
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({
+				mappings = {
+					-- NOTE: avoid conflicts with the built-in selection mappings on nvim 0.12
+					around_next = "aa",
+					inside_next = "ii",
+				},
+				n_lines = 500,
+			})
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      -- require("mini.surround").setup()
-      -- TODO: probably replace with surround with plugin
-    end,
-  },
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			-- require("mini.surround").setup()
+		end,
+	},
 }
