45b013f3ebfce5fd6945ae4d244e82eeb0f71f09
Robin Daubenschuetz <robinmeow97@gmail.com>
2026-04-27 13:43:41 +0200

    kitty: throttle resource usage on wsl

--- scripts/install-kitty.sh
+++ scripts/install-kitty.sh
@@ -8,3 +8,11 @@ run_on_fedora sudo dnf install -y kitty
 
 mkdir -p "$HOME/.config/kitty"
 cp -r "$PDE/.config/kitty/"* "$HOME/.config/kitty/"
+
+# NOTE: wsl is not as fast as native.
+# so im using a throttled kitty perf
+source "$PDE/scripts/detect_env.sh"
+detect_env
+if [[ "$OS_TYPE" == "wsl" ]]; then
+	cp -r "$PDE/.config/kitty/kitty-wsl.conf" "$HOME/.config/kitty/kitty.conf"
+fi
