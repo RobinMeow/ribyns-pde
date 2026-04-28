a448a3fa940a521f68a05efc83ecebef99bd07a2
Robin Daubenschuetz <robinmeow97@gmail.com>
2026-04-28 10:01:38 +0200

    rmpc: install timidity++ to fix error on mpd startup

--- scripts/install-rmpc.sh
+++ scripts/install-rmpc.sh
@@ -3,8 +3,8 @@
 set -u
 source "$PDE/scripts/run_on_distro.sh"
 
-run_on_arch sudo pacman -S --needed --noconfirm rmpc mpd
-run_on_fedora sudo dnf install -y rmpc mpd
+run_on_arch sudo pacman -S --needed --noconfirm rmpc mpd extra/timidity++
+run_on_fedora sudo dnf install -y rmpc mpd timidity++
 
 mkdir -p "$HOME/.config/rmpc"
 cp -r "$PDE/.config/rmpc/"* "$HOME/.config/rmpc/"
