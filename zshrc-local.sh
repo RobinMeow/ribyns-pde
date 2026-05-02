94eaaa8a0388dc02ed9d6e97a520ea1c56c37080
Ribyn <robinmeow97@gmail.com>
2026-05-02 20:08:49 +0200

    zshrc-local extend with function alias with auto completion

--- zshrc-local.sh
+++ zshrc-local.sh
@@ -11,3 +11,26 @@ fh() {
 	choice=$(cut -d';' -f2 "$HISTFILE" | sort -u | uniq | grep '..........' | fzf)
 	print -z "$choice"
 }
+
+# example for creating many aliases without worrying about conflicts or pollution or long startuptime:
+ribyn() {
+	# associative array (scoped to this fn)
+	declare -A temp_dirs=(
+		[env]="$HOME/ribyns-env/"
+		[state]="$HOME/ribyns-state/"
+	)
+
+	# Use the associative array to perform the "cd" operation
+	if [[ -n "$1" && -n "${temp_dirs[$1]}" ]]; then
+		cd "${temp_dirs[$1]}" || return 1
+	else
+		echo "ribyn: unknown key '$1'"
+		# echo "Available keys: ${(k)temp_dirs}" NOTE: this is zsh syntax and bashls doesnt like it
+		for k in "${!temp_dirs[@]}"; do
+			echo "  $k"
+		done
+		return 1
+	fi
+}
+# tab completion
+complete -W "env state" ribyn
