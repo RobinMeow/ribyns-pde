1af8c392559c3946967d56e518191f878d80ec66
Ribyn <robinmeow97@gmail.com>
2026-05-04 15:33:35 +0200

    add zshrc-local example for using fzf to cd

--- zshrc-local.sh
+++ zshrc-local.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # export RIBYNS_ENV_LOG_VERBOSE=true
 # export RIBYNS_ENV_LOG_INFO=true
 # export CODE_COMPANION_DEFAULT_ADAPTER="ollama"
@@ -8,3 +10,25 @@
 # export CDPATH="$HOME/Projects:$HOME/Projects/Domain:$CDPATH"
 # cd ProjectA (~/Projects/Domain/ProjectA)
 # cd Domain (~/Projects/Domain)
+
+# NOTE: example on navigating the filesystem for a specific env more easy using fzf
+#
+# customerabbr() {
+# 	# associative array (apparently scoped to this fn)
+# 	declare -A commands=(
+# 		[build]="dotnet build /p:NuGetAudit=false -v m"
+# 		[cd]="cd \$( (fd --max-depth 1 --type d . ~/customer; fd --max-depth 1 --type d . ~/customer/other-projects) | fzf )"
+# 	)
+#
+# 	if [[ -n "$1" && -n "${commands[$1]}" ]]; then
+# 		eval "${commands[$1]}"
+# 	else
+# 		echo "ribyn: unknown key '$1'"
+# 		for k in "${!commands[@]}"; do
+# 			echo "  $k"
+# 		done
+# 		return 1
+# 	fi
+# }
+# # tab completion
+# complete -W "build cd" ropa
